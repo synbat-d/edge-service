075a49bd952d2100360bee0bc338afe7cc81c671
Symbat Dulatuly <synbat1999@gmail.com>
2026-05-18 17:50:12 +0500

    Использование oauth2

--- src/main/java/com/polarbookshop/edge_service/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edge_service/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
         return http
                 .authorizeExchange(exchange ->
                         exchange.anyExchange().authenticated())
-                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults())
                 .build();
     }
 }
---
+++ src/main/java/com/polarbookshop/edge_service/user/User.java
@@ -0,0 +1,9 @@
+package com.polarbookshop.edge_service.user;
+
+import java.util.List;
+
+public record User(String username,
+                   String firstName,
+                   String lastName,
+                   List<String> roles) {
+}
