a18be7037e161d6bdf2910ebd9f9850670b01834
Symbat Dulatuly <synbat1999@gmail.com>
2026-05-18 21:28:00 +0500

    11.4 finish

--- src/main/java/com/polarbookshop/edge_service/user/UserController.java
+++ src/main/java/com/polarbookshop/edge_service/user/UserController.java
@@ -1,7 +1,6 @@
 package com.polarbookshop.edge_service.user;
 
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,16 +12,13 @@ import java.util.List;
 public class UserController {
 
     @GetMapping("/user")
-    public Mono<User> getUser(){
-        return ReactiveSecurityContextHolder
-                .getContext()
-                .map(SecurityContext::getAuthentication)
-                .map(authentication -> (OidcUser) authentication.getPrincipal())
-                .map(oidcUser -> new User(
-                        oidcUser.getPreferredUsername(),
-                        oidcUser.getGivenName(),
-                        oidcUser.getFamilyName(),
-                        List.of("employee", "customer")
-                ));
+    public Mono<User> getUser(@AuthenticationPrincipal OidcUser oidcUser) {
+        var user = new User(
+                oidcUser.getPreferredUsername(),
+                oidcUser.getGivenName(),
+                oidcUser.getFamilyName(),
+                List.of("employee", "customer")
+        );
+        return Mono.just(user);
     }
 }
