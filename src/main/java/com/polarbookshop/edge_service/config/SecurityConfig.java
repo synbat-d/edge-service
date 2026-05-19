a5c176059c8235905cd6a86cb06b0e256b749016
Symbat Dulatuly <synbat1999@gmail.com>
2026-05-19 21:52:10 +0500

    11.5 finish works properly

--- src/main/java/com/polarbookshop/edge_service/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edge_service/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.polarbookshop.edge_service.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
@@ -13,10 +14,12 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.ServerCsrfTokenRequestAttributeHandler;
 import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 
 @EnableWebFluxSecurity
+@Configuration
 public class SecurityConfig {
 
     @Bean
@@ -38,6 +41,7 @@ public class SecurityConfig {
                         .logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository)))
                 .csrf(csrf -> csrf
                         .csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(new ServerCsrfTokenRequestAttributeHandler())
                 )
                 .build();
     }
