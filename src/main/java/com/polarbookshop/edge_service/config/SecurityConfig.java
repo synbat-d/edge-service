872cc6d91f08c64384fcec6ef5b3fb0518b0c9f1
Symbat Dulatuly <synbat1999@gmail.com>
2026-05-19 17:43:03 +0500

    11.5 finish, but csrf token is not working properly

--- src/main/java/com/polarbookshop/edge_service/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edge_service/config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package com.polarbookshop.edge_service.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.web.server.csrf.CsrfToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.web.server.WebFilter;
+import reactor.core.publisher.Mono;
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
@@ -17,12 +24,21 @@ public class SecurityConfig {
                                                      ReactiveClientRegistrationRepository clientRegistrationRepository) {
         return http
                 .authorizeExchange(exchange ->
-                        exchange.anyExchange().authenticated())
+                        exchange
+                                .pathMatchers("/", "/*.css", "/*.js", "/favicon.ico")
+                                .permitAll()
+                                .pathMatchers(HttpMethod.GET, "/books/**")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated())
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling.authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .oauth2Login(Customizer.withDefaults())
                 .logout(logout -> logout
-                        .logoutSuccessHandler(
-                                oidcLogoutSuccessHandler(clientRegistrationRepository)
-                        ))
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository)))
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
+                )
                 .build();
     }
 
@@ -31,6 +47,22 @@ public class SecurityConfig {
         var oidcLogoutSuccessHandler = new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository);
         oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
         return oidcLogoutSuccessHandler;
+    }
+
+    @Bean
+    WebFilter csrfWebFilter() {
+        return (exchange, chain) -> {
+            exchange.getResponse().beforeCommit(
+                    () -> Mono.defer(
+                            () -> {
+                                Mono<CsrfToken> csrfToken =
+                                        exchange.getAttribute(CsrfToken.class.getName());
+                                return csrfToken != null ? csrfToken.then() : Mono.empty();
+                            }
+                    )
+            );
+            return chain.filter(exchange);
+        };
     }
 
 }
